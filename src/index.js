4ef56cda129ea88942f6b069c6b5d9eb6b6b6991
Steve Kinney <hello@stevekinney.net>
2017-03-03 13:07:32 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Application';
+import './index.css';
+
+ReactDOM.render(
+  <Application />,
+  document.getElementById('root')
+);
