0f83ef86b90927970942f2675a3ca063cfadfc55
Steve Kinney <hello@stevekinney.net>
2017-03-03 15:21:38 -0700

    Add users and fake data

--- src/initial-state.js
+++ src/initial-state.js
@@ -6,7 +6,30 @@ const initialState = {
     photoURL: null,
     uid: null
   },
-  messages: {}
+  messages: {
+    'firstMessage': {
+      content: 'My spoon is too big.',
+      timeStamp: Date.now() - 600,
+      uid: 'firstUser'
+    },
+    'secondMessage': {
+      content: 'I am a banana!',
+      timeStamp: Date.now() - 540,
+      uid: 'secondUser'
+    }
+  },
+  users: {
+    'firstUser': {
+      displayName: 'Bill Murray',
+      email: 'bill@example.com',
+      photoURL: 'http://www.fillmurray.com/200/200'
+    },
+    'secondUser': {
+      displayName: 'Place Kitten',
+      email: 'kitten@example.com',
+      photoURL: 'https://placekitten.com/g/200/200'
+    }
+  }
 };
 
 export default initialState;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import messagesReducer from './messages';
+import usersReducer from './users';
+
 
 const reducer = combineReducers({
   auth: authReducer,
-  messages: messagesReducer
+  messages: messagesReducer,
+  users: usersReducer
 });
 
 export default reducer;
