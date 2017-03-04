a39ac972ece74dc3e01397cd57722763443c6fad
Steve Kinney <hello@stevekinney.net>
2017-03-03 18:17:33 -0700

    Configure Firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyAhWMYWhBG2UvsNMZ-qVLbaqVZ5aTzwsNU",
+  authDomain: "hot-takes.firebaseapp.com",
+  databaseURL: "https://hot-takes.firebaseio.com",
+  storageBucket: "hot-takes.appspot.com",
+  messagingSenderId: "823384132241"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();
