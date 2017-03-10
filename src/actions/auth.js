b84fbd108a6500a49a92096fbd5a08af792f0a6e
Steve Kinney <hello@stevekinney.net>
2017-03-09 19:49:47 -0600

    Delay auth using a thunk

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,19 +1,38 @@
 export const signIn = () => {
-  return {
-    type: 'SIGN_IN',
-    email: 'bill@example.com',
-    displayName: 'Bill Murray',
-    photoURL: 'http://www.fillmurray.com/200/200',
-    uid: 'firstUser'
+  return (dispatch) => {
+    dispatch({ type: 'ATTEMPTING_LOGIN' });
+    setTimeout(() => {
+      dispatch(signedIn({
+        email: 'bill@example.com',
+        displayName: 'Bill Murray',
+        photoURL: 'http://www.fillmurray.com/200/200',
+        uid: 'firstUser'
+      }));
+    }, 2000);
   };
 };
 
 export const signOut = () => {
+  return (dispatch) => {
+    dispatch({ type: 'ATTEMPTING_LOGIN' });
+    setTimeout(() => {
+      dispatch(signedOut());
+    }, 2000);
+  };
+};
+
+const signedIn = (user) => {
   return {
-    type: 'SIGN_OUT',
-    email: null,
-    displayName: null,
-    photoURL: null,
-    uid: null
+    type: 'SIGN_IN',
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    uid: user.uid
+  };
+};
+
+const signedOut = () => {
+  return {
+    type: 'SIGN_OUT'
   };
 };
