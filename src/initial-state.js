af838fae6b0a57bdbca591a5bc38b77944271fe5
Steve Kinney <hello@stevekinney.net>
2017-03-10 07:00:18 -0600

    Remove initial state

--- src/initial-state.js
+++ src/initial-state.js
@@ -6,30 +6,8 @@ const initialState = {
     photoURL: null,
     uid: null
   },
-  messages: {
-    'firstMessage': {
-      content: 'My spoon is too big.',
-      timeStamp: Date.now() - 600,
-      uid: 'firstUser'
-    },
-    'secondMessage': {
-      content: 'I am a banana!',
-      timeStamp: Date.now() - 540,
-      uid: 'secondUser'
-    }
-  },
-  users: {
-    'firstUser': {
-      displayName: 'Bill Murray',
-      email: 'bill@example.com',
-      photoURL: 'http://www.fillmurray.com/200/200'
-    },
-    'secondUser': {
-      displayName: 'Place Kitten',
-      email: 'kitten@example.com',
-      photoURL: 'https://placekitten.com/g/200/200'
-    }
-  },
+  messages: {},
+  users: {},
   newMessage: ''
 };
 
